ced24ca297f44945e3cc73d2bed2e65b8714cfbc
albertaljeanpierre <albertaljeanpierre@gmail.com>
2022-12-08 20:57:04 +0100

    ajout d'un genre

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Genre;
+use App\Form\GenreType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +20,33 @@ class GenreController extends AbstractController
         $genres = $entityManager->getRepository(Genre::class)->findAll();
         return $this->render('genre/index.html.twig', [
              'genres' => $genres
+        ]);
+    }
+
+    /**
+     * @Route("/genre/new", name="app_genre_new")
+     */
+    public function ajoutGenre(EntityManagerInterface $entityManager , Request $request) : Response
+    {
+
+        $genre = new Genre();
+
+
+        $form = $this->createForm(GenreType::class, $genre );
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $genre = $form->getData();
+            $entityManager->persist($genre);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('app_genre');
+        }
+
+        return $this->render('genre/genreListe.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
